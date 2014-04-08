97e0e0808273d5f854947a4aa3c3ed15c2d94f47
Andreas Ölund <andreas@dartvoid.com>
2014-04-08 10:03:19 +0200

    Added Pure to the style mix

--- client/web/main.dart
+++ client/web/main.dart
@@ -1,21 +1,25 @@
 import 'dart:html';
 
 void main() {
-  var formInput  = querySelector("#form-input");
-  var formSubmit = querySelector("#form-submit");
-  var formClear  = querySelector("#form-clear");
-  var formResp   = querySelector("#form-response");
+  InputElement formInput  = querySelector("#form-input");
+  ButtonElement formSubmit = querySelector("#form-submit");
+  InputElement formClear  = querySelector("#form-clear");
+  DivElement formWrap   = querySelector("#response");
+  DivElement formResp   = querySelector("#response");
 
   formSubmit.onClick.listen((e) {
     e.preventDefault();
     
+    formWrap.classes = ["pure-u-1-1", "well"];
+    
     HttpRequest.request("/hello?name=${formInput.value}", method: "GET").then((response) {
-      formResp.appendHtml("<p>${response.responseText}</p>");
+      formResp.append(new ParagraphElement()..text = "${response.responseText}");
+      
     }).catchError((e) => formResp.appendHtml("<p>Unable to connect to the server</p>"));
   });
   
   formClear.onClick.listen((e) {
-    e.preventDefault();
+    formWrap.classes = ["pure-u-1-1"];
     formResp.children.clear();
   });
 }
