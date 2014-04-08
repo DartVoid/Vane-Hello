34721ed07e13abd077eda3ebd4e3f93c43d8e00a
Andreas Ölund <andreas@dartvoid.com>
2014-04-08 10:06:46 +0200

    Cleaned up source

--- client/web/main.dart
+++ client/web/main.dart
@@ -1,25 +1,25 @@
 import 'dart:html';
 
 void main() {
-  InputElement formInput  = querySelector("#form-input");
-  ButtonElement formSubmit = querySelector("#form-submit");
-  InputElement formClear  = querySelector("#form-clear");
-  DivElement formWrap   = querySelector("#response");
-  DivElement formResp   = querySelector("#response");
+  InputElement formInput    = querySelector("#form-input");
+  ButtonElement formSubmit  = querySelector("#form-submit");
+  InputElement formClear    = querySelector("#form-clear");
+  DivElement respWrap       = querySelector("#response");
+  DivElement formResp       = querySelector("#response");
 
   formSubmit.onClick.listen((e) {
     e.preventDefault();
-    
-    formWrap.classes = ["pure-u-1-1", "well"];
+    respWrap.classes = ["pure-u-1-1", "well"];
     
     HttpRequest.request("/hello?name=${formInput.value}", method: "GET").then((response) {
       formResp.append(new ParagraphElement()..text = "${response.responseText}");
-      
-    }).catchError((e) => formResp.appendHtml("<p>Unable to connect to the server</p>"));
+    }).catchError((e) {
+      formResp.appendHtml("<p>Unable to connect to the server</p>");
+    });
   });
   
   formClear.onClick.listen((e) {
-    formWrap.classes = ["pure-u-1-1"];
+    respWrap.classes = ["pure-u-1-1"];
     formResp.children.clear();
   });
 }
