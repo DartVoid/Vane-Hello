4de397ddc8135f046f92cf13b6eac83e24e8acb6
Andreas Ölund <andreas@dartvoid.com>
2014-04-10 09:52:43 +0200

    Added log messages

--- server/hello_world.dart
+++ server/hello_world.dart
@@ -4,8 +4,10 @@ import 'package:vane/vane.dart';
 class HelloWorld extends Vane {
   Future main() {
     if(query.containsKey("name")) {
+      log.info("Saying hello to ${query["name"]}");
       close("Hello ${query["name"]}");
     } else {
+      log.info("Saying hello to the world");
       close("Hello World!");  
     }
     
