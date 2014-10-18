15172da56368151c6ee538ce6feaca4a213f6a8d
Andreas Ölund <andreas@dartvoid.com>
2014-10-18 18:18:15 +0200

    Encode/decode messages to support UTF8 characters

--- client/web/main.dart
+++ client/web/main.dart
@@ -1,5 +1,5 @@
 import 'dart:html';
-import 'dart:convert';
+import 'dart:convert' show UTF8;
 import 'package:http/browser_client.dart';
 
 void main() {
@@ -16,10 +16,10 @@ void main() {
     Uri url = Uri.parse("/hello/${formInput.value}"); // Construct the request url
     
     // Send request
-    client.put(Uri.encodeFull(url.toString()), encoding: UTF8).asStream().listen((response) {
+    client.put(url).asStream().listen((response) {
       // Display response message
       formResponse.append(
-        new ParagraphElement()..text = "${Uri.decodeFull(response.body)}"
+        new ParagraphElement()..text = "${UTF8.decode(response.body.codeUnits)}"
       ); 
       // Catch errors
     }).onError((e) {
