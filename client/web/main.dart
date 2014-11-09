139892f9b0b2e97aff7676d6f499e82dc6ebffb1
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-11-09 17:53:25 +0100

    Updated so app works locally with CORS. Cleaned up the code and comments somewhat.

--- client/web/main.dart
+++ client/web/main.dart
@@ -8,32 +8,36 @@ void main() {
   InputElement formInput    = querySelector("#form-input");
   InputElement formClear    = querySelector("#form-clear");
   DivElement formResponse   = querySelector("#form-response");
-  
+
   // Send message
   formSubmit.onClick.listen((Event e) {
-    e.preventDefault(); // Prevent form submit
-    formResponse.classes.add("well"); // Style output wrapper
-    Uri url = Uri.parse("/hello/${formInput.value}"); // Construct the request url
-    
+    // Prevent form submit
+    e.preventDefault();
+
+    // Setup url (port 9090 for during development locally, otherwise use
+    // standard port 80 for production)
+    Uri uri = Uri.parse(window.location.href);
+    var port = uri.port != 8080 ? 80 : 9090;
+    Uri url = Uri.parse("http://${uri.host}:${port}/hello/${formInput.value}");
+
     // Send request
     client.put(Uri.encodeFull(url.toString()), encoding: UTF8).asStream().listen((response) {
       // Display response message
       formResponse.append(
         new ParagraphElement()..text = "${Uri.decodeFull(response.body)}"
-      ); 
+      );
       // Catch errors
     }).onError((e) {
       // Display an error message
       formResponse.append(
         new ParagraphElement()..text = "Unable to connect to the server"
-      ); 
+      );
     });
   });
-  
+
   // Clear input and output
   formClear.onClick.listen((Event e) {
-    formResponse.classes.remove("well"); // Remove output styling
-    formResponse.children.clear(); // Clear output
+    formResponse.children.clear();
   });
 }
 
